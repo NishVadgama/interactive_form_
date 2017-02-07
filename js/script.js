b3a0d2f3ad4b4a9c8326529095b293bddfe19773
Nish Vadgama <nishornash@gmail.com>
2017-02-07 16:21:45 -0500

    Other Filed Added - Part 2
    -when user selects 'other' from dropdown menu, a text area will
    dynmically be  added
    -when user selects different option, text area will be dynamically
    removed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-var $getRole = $("#title option:last").val();
+"use strict";
 
 //1. When the page loads, give focus to the first text field
 $("#name").focus();
 
 //2.  A text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
+//a) Crete a text field
+ $("#title").parent().append('<input type="text" id="other-field" placeholder="Your job title">');
+ $("#other-field").hide();
 
-$("#title option:last").click(function() {
-  
-  var textArea = $('<textarea style="padding-left:100px" />'); 
-  $getRole.append(textArea);
+//b) When 'other' is clicked, it will reveal Text Field
+$("#title").change(function() {
+	if($("#title option:selected").val() === "other"){
+		$("#other-field").show();
+	}else{
+		$("#other-field").hide();
+	}
 });
+
+
+
+
 
