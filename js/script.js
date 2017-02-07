675c7332e7b0d9bc166e8e5d32232f8ed5911da2
Nish Vadgama <nishornash@gmail.com>
2017-02-07 17:51:30 -0500

    T-Shirt Info Color Selection - Part 3
    -user can now select between 2 designs
    --each design has 3 differnt colors unique to the designs

--- js/script.js
+++ js/script.js
@@ -10,14 +10,37 @@ $("#name").focus();
 
 //b) When 'other' is clicked, it will reveal Text Field
 $("#title").change(function() {
-	if($("#title option:selected").val() === "other"){
+	if($("select#title").val() === "other"){
 		$("#other-field").show();
 	}else{
 		$("#other-field").hide();
 	}
 });
 
+//3.  T-Shirt Info” section of the form:
+// If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue," "Dark Slate Grey," and "Gold."
 
+// If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato," "Steel Blue," and "Dim Grey."
 
+$("#color").prepend('<option value="selectColor" selected>Select T-Shirt Color</option>');
 
+$("#design").change(function() {
+	if($("select#design").val() === "js puns"){		
+		$( "#color option[value='tomato']").hide();
+		$( "#color option[value='steelblue']").hide();
+		$( "#color option[value='dimgrey']").hide();
+		$( "#color option[value='gold']").show();
+		$( "#color option[value='cornflowerblue']").show();
+		$( "#color option[value='darkslategrey']").show();
+	}
+	if($("select#design").val() === "heart js"){
+		$( "#color option[value='gold']").hide();
+		$( "#color option[value='cornflowerblue']").hide();
+		$( "#color option[value='darkslategrey']").hide();
+		$( "#color option[value='tomato']").show();
+		$( "#color option[value='steelblue']").show();
+		$( "#color option[value='dimgrey']").show();
 
+	}
+
+});
